01c05dd3f3198cea911419540cc3b71da7c4f5b2
4Mago <marcus.bjorsson@gmail.com>
2020-10-22 16:07:50 +0200

    gjort routing i nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from './components/header'
 import Brollop from './pages/brollop.component'
 import { Route, Switch } from 'react-router-dom'
 import Footer from "./components/footer.component"
+import Begravning from "./pages/begravning.component"
+import Buketter from "./pages/buketter.component"
+import Kontakt from "./pages/kontakt.component"
 
 // let goingUp = useRef(null)
 // let GoingDown2 = useRef(null)
@@ -16,6 +19,15 @@ const App = () => (
         </Route>
         <Route path={'/brollop'}>
           <Brollop />
+        </Route>
+        <Route path={'/begravning'}>
+          < Begravning />
+        </Route>
+        <Route path={'/buketter'}>
+          <Buketter />
+        </Route>
+        <Route path={'/kontakt'}>
+          <Kontakt />
         </Route>
       <Footer />
     </div>
---
+++ src/pages/buketter.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Buketter = () => {
+    return (
+        <div>
+            <h1>Buketter</h1>
+        </div>
+    )
+}
+
+export default Buketter
