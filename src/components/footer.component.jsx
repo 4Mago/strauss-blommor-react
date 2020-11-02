23f031a9040aa2c399f939fd4348a9c016fb51c3
blixt <teodor.blixt@gmail.com>
2020-11-02 16:09:30 +0100

    fix footer

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,95 +1,87 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Container = styled.div`
-clear: both;
+	height: auto;
+	background: #273420;
+	color: #ffffff;
+`
+const NavContainer = styled.div`
 display: flex;
 justify-content: space-between;
-height: 100px;
-bottom: 0;
-background: #273420;
-color: #ffffff;
+height: auto;
 }
 `
 
 const Socials = styled.div`
-display: flex;
-justify-content: flex-start;
-padding: 15px;
-height: 150px;
-background: #273420;
-flex-direction: column;
-color: #ffffff;
+	display: flex;
+	align-items: flex-start;
+	justify-content: flex-start;
+	padding: 15px;
+	height: 125px;
+	flex-direction: column;
 `
 
 const Img = styled.img`
-  height: 50px;
-  width: 50px;
-  padding: 2px;
+	height: 50px;
+	width: 50px;
+	padding: 2px;
 `
-
-const Left = styled.div`
-  padding: 15px;
-  text-align: left;
-  font-family: poppins;
+const InfoContainer = styled.div`
+	padding: 15px;
+	display: flex;
+	flex-flow: column;
+	height: auto;
 `
-const CenterLeft = styled.div`
-padding: 15px;
-
-`
-
-const CenterRight = styled.div`
-padding: 15px;
-
-`
-
-const Right = styled.div`
-  padding: 20px;
-  text-align: right;
-  font-family: poppins;
+const InfoHeader = styled.h4``
+const InfoText = styled.p``
+const InfoLink = styled(Link)`
+	text-decoration: none;
+	color: white;
 `
 
 const Footer = () => {
-    return (
-      <div>
-  <Container>
-    <Left>
-    <h4>Öppettider</h4>
-        <br/>  Måndag - Fredag 10.00 - 18.30
-        <br/>  Lördag 10.00 - 16.00
-    </Left>
-    <CenterLeft>
-      <h4>Navigation</h4>
-      Buketter
-      <br />Begravning
-      <br />Bröllop
-      <br />Kontakt
-    </CenterLeft>
-    <div className='middle'></div>
-    <CenterRight>
-      <h4>Företagstjänster</h4>
-      Beställningar
-      <br />Event
-      <br />
-    </CenterRight>
-    <Right>
-    <h4>Adress</h4>
-    Strauss Blommor
-    <br />Rosenlundsgatan 32 C
-    <br />118 53 Stockholm
-    </Right>
-    </Container>
-    <Socials>
-      <br></br>
-      <h2>Socials</h2>
-      <div>
-      <Img alt='hej' src='/images/facebook.png'></Img>
-      <Img alt='hej' src='/images/instagram.png'></Img>
-      <Img alt='hej' src='/images/linkedin.png'></Img>
-      </div>
-    </Socials>
-    </div>
-    )
+	return (
+		<Container>
+			<NavContainer>
+				<InfoContainer>
+					<InfoHeader>Öppettider</InfoHeader>
+					<InfoText>
+						Måndag - fredag 10.00 - 18.30 <br></br> Lördag 10.00 - 16.00
+					</InfoText>
+				</InfoContainer>
+				<InfoContainer>
+					<InfoHeader>Navigation</InfoHeader>
+					<InfoLink to='/buketter'>Buketter</InfoLink>
+					<InfoLink to='/begravning'>Begravning</InfoLink>
+					<InfoLink to='/brollop'>Bröllop</InfoLink>
+					<InfoLink to='/kontakt'>Kontakt</InfoLink>
+				</InfoContainer>
+				<InfoContainer>
+					<InfoHeader>Företagstjänster</InfoHeader>
+					<InfoLink to='/bestallningar'>Beställningar</InfoLink>
+					<InfoLink to='/event'>Event</InfoLink>
+				</InfoContainer>
+				<InfoContainer>
+					<InfoHeader>Adress</InfoHeader>
+					<InfoText>
+						Strauss Blommor <br></br>
+						Rosenlundsgatan 32 C <br></br>
+						118 53 Stockholm
+					</InfoText>
+				</InfoContainer>
+			</NavContainer>
+			<Socials>
+				<h2>Socials</h2>
+				<div>
+					<Img alt='hej' src='/images/facebook.png'></Img>
+					<Img alt='hej' src='/images/instagram.png'></Img>
+					<Img alt='hej' src='/images/linkedin.png'></Img>
+				</div>
+			</Socials>
+		</Container>
+	)
 }
 
 export default Footer
