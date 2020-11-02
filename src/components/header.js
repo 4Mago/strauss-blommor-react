ee455bbbada034fc05867c7404f3b6aa2eb08b2c
blixt <teodor.blixt@gmail.com>
2020-11-02 13:44:18 +0100

    åäö

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../components/the-big-file.css'
 import { Link } from 'react-router-dom'
 import sanityClient from '../Client'
@@ -11,90 +11,94 @@ function urlFor(source) {
 }
 
 const ImgContainer = styled.div`
-background: white;
-margin-top: 19px;
-display: flex;
-justify-content: center;
-align-items: center;
+	background: white;
+	margin-top: 19px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `
 
 const NavContainer = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-height: 60px;
-text-decoration: none;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 60px;
+	text-decoration: none;
 
-@media screen and (max-width: 700px) {
-  width: 100%;
-  padding-right: 8px;
-  padding-left: 8px;
-}
+	@media screen and (max-width: 700px) {
+		width: 100%;
+		padding-right: 8px;
+		padding-left: 8px;
+	}
 
-@media screen and (max-width: 500px) {
-
-}
+	@media screen and (max-width: 500px) {
+	}
 `
 
 const NavLinks = styled.div`
-display: flex;
-font-family: 'Poppins', sans-serif;
-font-size: 1em;
-font-weight: 400;
-color: black;
-justify-content: space-between;
-gap: 1.5em;
+	display: flex;
+	font-family: 'Poppins', sans-serif;
+	font-size: 1em;
+	font-weight: 400;
+	color: black;
+	justify-content: space-between;
+	gap: 1.5em;
 
-@media screen and (max-width: 700px) {
-  gap: 10px;
-}
+	@media screen and (max-width: 700px) {
+		gap: 10px;
+	}
 
-@media screen and (max-width: 500px) {
-
-}
+	@media screen and (max-width: 500px) {
+	}
 `
 
-
-
 const Header = () => {
+	const [header, setHeader] = useState('')
 
-  const [header, setHeader] = useState('')
+	useEffect(() => {
+		const headerQuery = `*[_type == "header"]`
 
-  useEffect(() => {
-      const headerQuery = `*[_type == "header"]`
+		sanityClient.fetch(headerQuery).then(header => {
+			header.forEach(header => {
+				setHeader(header)
+			})
+		})
+		return
+	}, [])
 
-      sanityClient.fetch(headerQuery).then(header => {  
-         header.forEach(header => {
-           setHeader(header)
-         })
-      })
-      return
-  }, [])
-  
-return (
-  <header>
-  <ImgContainer>
-    <Link to="/">
-    <img alt="logo" src={urlFor(header.logo).url()} />
-    </Link>
-
-   
-        </ImgContainer>
-    <NavContainer>
-      <NavLinks>
-      
-      {header.menu ? header.menu.map((item, id) => {
-        return (  <Link key={id} className='menu-item' to={`/${item.toLowerCase()}`}>{item}</Link>)
-      }) : null}
-
-      </NavLinks>
-    </NavContainer>
-</header>
-
-)
- 
-
+	return (
+		<header>
+			<ImgContainer>
+				<Link to='/'>
+					<img alt='logo' src={urlFor(header.logo).url()} />
+				</Link>
+			</ImgContainer>
+			<NavContainer>
+				<NavLinks>
+					{header.menu
+						? header.menu.map((item, id) => {
+								return (
+									<Link
+										key={id}
+										className='menu-item'
+										to={`/${item
+											.replace(/å/g, 'a')
+											.replace(/Å/g, 'a')
+											.replace(/ä/g, 'a')
+											.replace(/Ä/g, 'a')
+											.replace(/ö/g, 'o')
+											.replace(/Ö/g, 'o')
+											.toLowerCase()}`}
+									>
+										{item}
+									</Link>
+								)
+						  })
+						: null}
+				</NavLinks>
+			</NavContainer>
+		</header>
+	)
 }
- 
 
 export default Header
