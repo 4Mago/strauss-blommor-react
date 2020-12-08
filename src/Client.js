e40cd15f30e64923e271492db658c97c3855f3b2
4Mago <47850942+4Mago@users.noreply.github.com>
2020-12-08 22:29:54 +0100

    hello

--- src/Client.js
+++ src/Client.js
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client'
 
 
 export default sanityClient({
-  projectId: '4ev62qhg',
+  projectId: 'p7ossqve',
   dataset: 'production',
   token: '', // or leave blank to be anonymous user
   useCdn: true // `false` if you want to ensure fresh data
