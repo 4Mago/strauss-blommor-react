fba6a67171ac9e231e001a30f691908a5866a14a
4Mago <47850942+4Mago@users.noreply.github.com>
2020-12-15 03:33:10 +0100

    push

---
+++ src/components/header/Tagline.js
@@ -0,0 +1,41 @@
+import React, { useContext } from 'react'
+import { HomeContext } from '../../store/home-context.component'
+import styled from 'styled-components'
+
+const TaglineDiv = styled.div`
+    width: 100%;
+    height: 500px;
+    display: flex;
+    flex-flow: column;
+    gap: 22px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+`
+
+const TaglineTitle = styled.h2`
+`
+
+const TaglineText = styled.p`
+    color: black;
+    width: 550px;
+`
+
+
+
+const Tagline = () => {
+
+    const { home } = useContext(HomeContext)
+
+    return (
+        <>
+                    
+            <TaglineDiv>
+    <TaglineTitle>{home.title}</TaglineTitle>
+    <TaglineText>{home.description2}</TaglineText>
+        </TaglineDiv>
+        </>
+    )
+}
+
+export default Tagline
--- src/pages/home.component.jsx
+++ src/pages/home.component.jsx
@@ -5,6 +5,7 @@ import OpenHours from '../components/Misc/openHours'
 import styled from 'styled-components'
 import GoogleMaps from '../components/Misc/GoogleMaps'
 import Header from '../components/header/header'
+import Tagline from '../components/header/Tagline'
 
 const OuterContainer = styled.div`
 	width: 100%;
@@ -21,6 +22,7 @@ const Home = () => {
 			<OuterContainer>
 					<Header />				
 			</OuterContainer>
+			<Tagline />
 			<SliderShow />
 			<OpenHours />
 			<GoogleMaps />
